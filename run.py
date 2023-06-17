b63362241e9588e2c611e1d6db21fd5382152729
VictoriaParkes <>
2023-06-17 09:58:52 +0000

    Calculate answer percentages

--- run.py
+++ run.py
@@ -15,21 +15,35 @@ SHEET = GSPREAD_CLIENT.open('RSVP_Responses')
 """ Add variable for worksheet """
 responses_worksheet = SHEET.worksheet('Responses')
 
-def responses_total():
+
+def responses_total_calc(col):
     """
     Calculate the total number of rows of data in the worksheet.
     """
-    total_rows = len(responses_worksheet.col_values(4)[1:])
-    blank_rows = responses_worksheet.col_values(4).count("")
+    total_rows = len(responses_worksheet.col_values(col)[1:])
+    blank_rows = responses_worksheet.col_values(col).count("")
     responses = total_rows - blank_rows
-    print(f'The invitation received a total of {responses} responses.')
+    return responses
+
+
+def question_responses(col):
+    possible_answers = set((responses_worksheet.col_values(col)[1:]))
+    possible_answers.remove("")
+    all_column_values = list((responses_worksheet.col_values(col)[1:]))
+    total_responses = responses_total_calc(col)
+    for answer in possible_answers:
+        answer_percent = (all_column_values.count(answer)/total_responses)*100
+        print(f"{answer} = {answer_percent}%")
 
 
 def analysis():
     """
     Run all analysis functions.
     """
-    responses_total()
+    responses_total = responses_total_calc(1)
+    print(f'The invitation received a total of {responses_total} responses.')
+    question_responses(4)
+    question_responses(6)
 
 
 analysis()
