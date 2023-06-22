bd2756a8861d41ccf9c4b4db701c8dea3f1d8ff1
VictoriaParkes <>
2023-06-22 14:54:55 +0000

    Add question processing loop

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 import gspread
+import itertools
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -21,9 +22,9 @@ def main_menu():
     print('1. RSVP Response Data Analysis')
     print('2. Question/Comment Manager\n')
     print("Select an option by entering 1 or 2\n")
-    
     while True:
-        selection = input("Enter your choice here and press enter to continue:\n")
+        selection = input("Enter your choice here"
+                          " and press enter to continue:\n")
         if validate_menu_selection(selection):
             if selection == '1':
                 analysis()
@@ -94,5 +95,124 @@ def analysis():
     question_responses(6)
 
 
-print('Welcome to the RSVP Response Manager.\n')
-main_menu()
+def email_response():
+    print('email response')
+
+
+def ignore_question():
+    print('ignore')
+
+
+def skip_question():
+    print('skip')
+
+
+def input_required():
+    print(
+            "Review the question/comment recieved "
+            "and choose an appropriate action\n"
+        )
+    print("To respond to the question/comment enter 1")
+    print("To mark the question/comment as ignored enter 2")
+    print(
+        "To skip to the next question/comment "
+        "without processing enter 3"
+    )
+    print("To exit to the main menu enter 4\n")
+    print("Press enter to continue\n")
+    while True:
+        action_selection = input("Enter a number between 1 and 4 here:\n")
+
+        if validate_data(action_selection):
+            if action_selection == '1':
+                email_response()
+            elif action_selection == '2':
+                ignore_question()
+            elif action_selection == '3':
+                skip_question()
+            elif action_selection == '4':
+                print('Exiting to main menu...')
+                main_menu()
+            break
+
+
+def validate_data(value):
+    """
+    REWRITE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
+    REWRITE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    """
+    try:
+        selection = int(value)
+        if selection > 4 or selection < 1:
+            raise ValueError(
+                f"Please enter a number from 1 - 4, you entered {value}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
+
+
+def question_asked():
+    """
+    Create a list of responses with questions or comments
+    that have not been responded to or ignored,
+    as dictionaries with headings as key and data as value.
+    """
+    data_vals = SHEET.get_all_values()
+    headings = data_vals[0]
+    headings.insert(0, 'row')
+    del data_vals[0]
+    headings_vals_dicts = []
+    row_num = 2
+    for row in data_vals:
+        row.insert(0, row_num)
+        row_num += 1
+        zipped = dict(zip(headings, row))
+        headings_vals_dicts.append(zipped)
+
+    asked_question = [
+        row for row in headings_vals_dicts if not (
+            row["Comments/questions"] == ""
+        ) and not (
+            row["Responded/ignored"] == "Responded"
+        ) and not (
+            row["Responded/ignored"] == "Ignored"
+        )
+    ]
+    return asked_question
+
+
+def view_questions(data):
+    """
+    Check if list contains any response data items,
+    iterate through the list, print the response data
+    and run function to process question/comment or
+    print a message informing the user that there are
+    no questions/comments to review.
+    """
+    if len(data) >= 1:
+        for row in data:
+            top_of_list = dict(itertools.islice(row.items(), 0, 8))
+            for key in top_of_list:
+                print(f"{key}: {top_of_list[key]}")
+            print('\n')
+            input_required()
+        print('No more questions.')
+        print('You have reached the end of the list.')
+        input("Press the Enter key to return to the main menu: ")
+    else:
+        print('There are currently no questions/comments to review.')
+        input("Press the Enter key to return to the main menu: ")
+        main_menu()
+
+
+# print('Welcome to the RSVP Response Manager.\n')
+# main_menu()
+
+question_rows = question_asked()
+view_questions(question_rows)
